f6733f052ad919b426b1a5c8f0d53be1df347666
Alexander Paz <alexjpaz@gmail.com>
2020-05-08 15:22:00 -0400

    Adding a way to get ffmpeg, ffprob, and a font

---
+++ scripts/download-dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+mkdir -p opt
+pushd opt
+
+
+curl -sL https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar x
+find .
+cp $(find . -iname ffmpeg | head -1) ffmpeg
+cp $(find . -iname ffprobe | head -1) ffprobe
+rm -rf ffmpeg-*
+
+curl -sL 'https://github.com/google/fonts/blob/master/apache/opensans/OpenSans-Bold.ttf?raw=true' > OpenSans-Bold.ttf
