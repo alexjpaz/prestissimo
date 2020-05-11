6fd7b4d07c0a64a8542bbbd069a56b812d2a99f4
Alexander Paz <alexjpaz@gmail.com>
2020-05-11 19:49:04 -0400

    http upload tests

--- src/http/router.js
+++ src/http/router.js
@@ -1,10 +1,48 @@
+const config = require('config');
 const express = require('express');
+const multer  = require('multer');
 
-const Router = () => {
+const os = require('os');
+
+const AWS = require('../utils/aws');
+const { logger } = require('../utils/logger');
+
+const fs = require('fs').promises;
+
+const defaultProps = () => ({
+  s3: new AWS.S3()
+});
+
+const Router = (props = defaultProps()) => {
+  const { s3 } = props;
+
   const app = express();
 
-  app.post('/upload', (req, res) => {
-    res.send(201);
+  const upload = multer({ dest: os.tmpdir() })
+
+  app.post('/upload', upload.array('file', 10), async (req, res, next) => {
+    try {
+
+      const promises = req.files
+        .map(async (file) => {
+          try {
+            await s3.putObject({
+              Bucket: config.awsBucket,
+              Key: `uploads/raw/${file.filename}/${file.originalname}`,
+              Body: await fs.readFile(file.path)
+            }).promise();
+          } finally {
+            await fs.unlink(file.path);
+          }
+        });
+
+      await Promise.all(promises);
+
+      return res.sendStatus(201);
+    } catch(e) {
+      logger.error(e);
+      return next(e);
+    }
   });
 
   return app;
--- src/http/router.spec.js
+++ src/http/router.spec.js
@@ -1,18 +1,117 @@
+const config = require('config');
+const sinon = require('sinon');
+const { expect } = require('chai');
+const supertest = require('supertest');
+
+const fs = require('fs').promises;
+
 const Router = require('./router');
 
-const superagent = require('superagent');
-it('ssssssss', () => {});
+const AWS = require('../utils/aws');
 
 describe('http/router', () => {
   let request;
 
+  let mockS3;
+
   beforeEach(() => {
-    request = superagent(Router());
+    mockS3 = new AWS.S3();
+
+    sinon.spy(mockS3, 'putObject');
+
+    request = supertest(Router({
+      s3: mockS3
+    }));
+
+    sinon.spy(fs, 'unlink');
   });
 
-  it('1', async () => {
-    await request.post('/upload')
-      .send("1");
-    ;
+  afterEach(() => {
+    fs.unlink.restore();
+  });
+
+  describe('upload', () => {
+    it('m4a file', async () => {
+      await request.post('/upload')
+        .set('Content-Type', 'multipart/form-data')
+        .type('form')
+        .attach("file", "./test/Beachy.m4a")
+        .expect(201);
+      ;
+
+      expect(fs.unlink.calledOnce).to.eql(true);
+
+      expect(mockS3.putObject.calledOnce).to.eql(true);
+
+      const params = mockS3.putObject.args[0][0];
+
+      expect(params.Key).to.match(/Beachy\.m4a$/);
+      expect(params.Body).to.be.a('Uint8Array');
+      expect(params.Body.length).to.eql(538295);
+
+      const headRsp = await mockS3.headObject({
+        Bucket: params.Bucket,
+        Key: params.Key,
+      }).promise();
+
+      expect(headRsp.ContentLength).to.eql(538295);
+    });
+
+    it('multiple files', async () => {
+      await request.post('/upload')
+        .set('Content-Type', 'multipart/form-data')
+        .type('form')
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .expect(201);
+      ;
+
+      const calls = mockS3.putObject.getCalls();
+
+      expect(calls.length).to.eql(10);
+      expect(fs.unlink.getCalls().length).to.eql(10);
+
+      for(let call of calls) {
+        const params = call.args[0];
+
+        expect(params.Key).to.match(/Beachy\.m4a$/);
+        expect(params.Body).to.be.a('Uint8Array');
+        expect(params.Body.length).to.eql(538295);
+
+        const headRsp = await mockS3.headObject({
+          Bucket: params.Bucket,
+          Key: params.Key,
+        }).promise();
+
+        expect(headRsp.ContentLength).to.eql(538295);
+      }
+    });
+
+    it('multiple files exceed maximum', async () => {
+      await request.post('/upload')
+        .set('Content-Type', 'multipart/form-data')
+        .type('form')
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .attach("file", "./test/Beachy.m4a")
+        .expect(500);
+      ;
+    });
   });
 });
