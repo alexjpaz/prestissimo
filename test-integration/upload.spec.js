5bb92b5b0a5bc539a2a5f035a39cafb73a3e9547
Alexander Paz <alexjpaz@gmail.com>
2020-05-12 17:50:35 -0400

    Fixing bucket params

--- test-integration/upload.spec.js
+++ test-integration/upload.spec.js
@@ -2,11 +2,26 @@ const supertest = require('supertest');
 
 let request = supertest(process.env.BASE_URL);
 
-it('upload', async () => {
-  await request.post('/upload')
-        .set('Content-Type', 'multipart/form-data')
-        .type('form')
-        .attach("file", Buffer.alloc(0))
-        .expect(201)
-  ;
+describe('upload', () => {
+
+  it('zero byte', async () => {
+    const rsp = await request.post('/upload')
+      .set('Content-Type', 'multipart/form-data')
+      .type('form')
+      .attach("file", Buffer.alloc(0))
+      .expect(201)
+    ;
+  });
+
+  it('file', async () => {
+    const rsp = await request.post('/upload')
+      .set('Content-Type', 'multipart/form-data')
+      .type('form')
+      .attach("file", "./test/Beachy.m4a")
+      .then((rsp) => console.log(rsp))
+      .expect(201)
+    ;
+
+    console.log(rsp);
+  });
 });
