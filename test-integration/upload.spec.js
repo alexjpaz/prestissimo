dfb712053f0b97e724695b564cbc5693f9c8a99b
Alexander Paz <alexjpaz@gmail.com>
2020-05-16 14:52:07 -0400

    Fixing integration tests

--- test-integration/upload.spec.js
+++ test-integration/upload.spec.js
@@ -1,20 +1,49 @@
+const { expect } = require('chai');
+
 const supertest = require('supertest');
+const superagent = require('superagent');
 
 let request = supertest(process.env.BASE_URL);
 
 describe('upload', () => {
 
-  it('should create a signed PUT url', () => {
-    rsp = await request.post('/upload/signed-url')
+  it('should create a signed PUT url', async () => {
+    rsp = await request.put('/upload/signed-url')
       .expect(200);
 
-    expect(rsp).to.eql(1);
+    const url = rsp.text;
+
+    expect(url).to.include('prestissimo');
+    expect(url).to.include('x-amz-security-token');
+
+    const buffer = Buffer.from("test");
+
+    const req = await superagent.put(url)
+      .send(buffer);
   })
 
-  it('should create a signed POST url', () => {
+  it('should create a signed POST url', async () => {
     rsp = await request.post('/upload/signed-url')
       .expect(200);
 
-    expect(rsp).to.eql(1);
+    let data = rsp.body;
+
+    const req = superagent.post(data.url);
+
+    req.type('form');
+
+    Object.keys(data.fields).map((key) => {
+      req.field(key, data.fields[key]);
+    });
+
+    req.field("key", "uploads/raw/test");
+
+    const buffer = Buffer.from("test23");
+
+    req.attach("file", buffer)
+
+    let { statusCode } = await req;
+
+    expect(statusCode).to.eql(204);
   })
 });
