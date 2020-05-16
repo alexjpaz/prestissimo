ce5bf761182b870ba57ae4246022732ddc60659f
Alexander Paz <alexjpaz@users.noreply.github.com>
2020-05-16 15:13:17 -0400

    Use pre signed urls for uploading objects (#11)
    * Add CORS policy for staging bucket
    
    * Removing old upload and replacing with presigned urls
    
    * Removing errouneous provider key
    
    * Use proper casing
    
    Perhaps I misread the documentation? https://www.serverless.com/framework/docs/providers/aws/events/s3/#custom-bucket-configuration
    
    * Fixing integration tests

--- test-integration/upload.spec.js
+++ test-integration/upload.spec.js
@@ -1,27 +1,49 @@
+const { expect } = require('chai');
+
 const supertest = require('supertest');
+const superagent = require('superagent');
 
 let request = supertest(process.env.BASE_URL);
 
 describe('upload', () => {
 
-  it('zero byte', async () => {
-    const rsp = await request.post('/upload')
-      .set('Content-Type', 'multipart/form-data')
-      .type('form')
-      .attach("file", Buffer.alloc(0))
-      .expect(201)
-    ;
-  });
+  it('should create a signed PUT url', async () => {
+    rsp = await request.put('/upload/signed-url')
+      .expect(200);
 
-  it('file', async () => {
-    const rsp = await request.post('/upload')
-      .set('Content-Type', 'multipart/form-data')
-      .type('form')
-      .attach("file", "./test/Beachy.m4a")
-      .expect((rsp) => {
-        console.log(rsp.text);
-      })
-      .expect(201)
-    ;
-  });
+    const url = rsp.text;
+
+    expect(url).to.include('prestissimo');
+    expect(url).to.include('x-amz-security-token');
+
+    const buffer = Buffer.from("test");
+
+    const req = await superagent.put(url)
+      .send(buffer);
+  })
+
+  it('should create a signed POST url', async () => {
+    rsp = await request.post('/upload/signed-url')
+      .expect(200);
+
+    let data = rsp.body;
+
+    const req = superagent.post(data.url);
+
+    req.type('form');
+
+    Object.keys(data.fields).map((key) => {
+      req.field(key, data.fields[key]);
+    });
+
+    req.field("key", "uploads/raw/test");
+
+    const buffer = Buffer.from("test23");
+
+    req.attach("file", buffer)
+
+    let { statusCode } = await req;
+
+    expect(statusCode).to.eql(204);
+  })
 });
