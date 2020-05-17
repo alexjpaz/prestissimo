c9a6f75beef769b59722e7e076870c9cd9c6a81b
Alexander Paz <alexjpaz@gmail.com>
2020-05-16 21:04:49 -0400

    Stubbing out integration test for tokens

--- test-integration/auth.spec.js
+++ test-integration/auth.spec.js
@@ -9,9 +9,25 @@ describe('auth', () => {
     ;
   });
 
-  it('should bounce when no credentials are provided', async () => {
-    await request.get('/some-private-url')
-      .expect(401)
-    ;
+  describe('unauthorized', () => {
+    it('should bounce when no credentials are provided', async () => {
+      await request.get('/some-private-url')
+        .expect(401)
+      ;
+    });
+  });
+
+  describe('authorized', () => {
+    let token;
+    before(() => {
+      // TODO - get token!
+    });
+
+    it('should allow', async () => {
+      await request.post('/some-private-url')
+        .set("Authorization", `Bearer ${token}`)
+        .expect(404)
+      ;
+    });
   });
 });
