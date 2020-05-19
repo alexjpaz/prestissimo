5a6b33e587fa874ea6d3077d12ee0455f21486b9
Alexander Paz <alexjpaz@gmail.com>
2020-05-18 20:43:36 -0400

    Fixing tests

--- ui/index.spec.js
+++ ui/index.spec.js
@@ -1,14 +1,29 @@
 beforeAll(() => {
+  let root = document.createElement('div');
+  root.id = 'app';
+
+  let loader = document.createElement('div')
+  loader.id = 'loader';
+
+  document.body.appendChild(root);
+  document.body.appendChild(loader);
 });
 
 it('should mount', () => {
   let root = null;
+  let loader = null;
 
   root = document.querySelectorAll('[data-test-id="App-root"]');
+  loader = document.querySelectorAll('#loader');
+
   expect(root.length).toEqual(0);
+  expect(loader.length).toEqual(1);
 
   require('./');
 
   root = document.querySelectorAll('[data-test-id="App-root"]');
+  loader = document.querySelectorAll('#loader');
+
   expect(root.length).toEqual(1);
+  expect(loader.length).toEqual(0);
 });
