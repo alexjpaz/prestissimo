827568565d4e7bca9c66ad3cd9c3c2d997da6827
Alexander Paz <alexjpaz@gmail.com>
2020-05-23 09:08:36 -0400

    Adding local server stories

--- ui/components/App.js
+++ ui/components/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import { UploadForm } from './UploadForm';
 
+import { AppContext } from './AppContext';
+
 export function App() {
+  const ctx = React.useContext(AppContext);
+
   return (
     <div data-test-id='App-root'>
-      <UploadForm />
+      <h1>client = {ctx.user.name}</h1>
+      <UploadForm onUpload={ctx.uploadTrack} />
     </div>
   );
 }
--- ui/components/App.stories.js
+++ ui/components/App.stories.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
+
 import { App } from './App';
+
+import { AppContext } from './AppContext';
 
 export default {
   title: 'App',
 };
 
-export const withDefault = () => (
-  <App />
-);
+export const withMockServer = () => {
+  let value = {
+    user: {
+      name: "Test User",
+    },
+    uploadTrack: action('uploadTrack')
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      <App />
+    </AppContext.Provider>
+  );
+};
+
+export const withLocalServer = () => {
+  // TODO
+  let value = {
+
+    user: {
+      name: "Test User",
+    },
+    uploadTrack: action('uploadTrack')
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      <App />
+    </AppContext.Provider>
+  );
+};
