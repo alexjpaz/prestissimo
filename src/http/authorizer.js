66c4df0a5204f5d549ce473caca8fdcdf201463f
Alexander Paz <alexjpaz@gmail.com>
2020-05-25 10:49:57 -0400

    Fixing authorizer

--- src/http/authorizer.js
+++ src/http/authorizer.js
@@ -1,6 +1,12 @@
+const request = require('superagent');
+
+const { JWT, JWKS } = require('jose');
+
 const jwksClient = require('jwks-rsa');
 const jwt = require('jsonwebtoken');
 const util = require('util');
+
+const fs = require('fs').promises;
 
 const config = require('config');
 
@@ -44,45 +50,79 @@ const Authorizer = (props = config.authorizer) => {
     issuer: props.issuer,
   };
 
-  const authenticate = (params) => {
-    //// TOOOOOODOOOOOOOOOOOO
-    // FIXME
-    if(props.usePassthrough) {
-      logger.warn("DEBUG MODE USING PASSTHROUGH AUTH");
-      return Promise.resolve({
-        principalId: "TEST_PRINCIPAL_ID",
-        policyDocument: getPolicyDocument('Allow', params.methodArn),
-        context: { scope: ['stuff'] }
-      });
+  let cachedKeyStore;
+  let cacheTimeout = 60 * 1000; // 60 seconds
+  let expiresAt;
+
+  const getKeyStore = async (jwksUri = props.jwksUri) => {
+    const now = new Date();
+
+    if(cachedKeyStore) {
+      if(now < expiresAt) {
+        return cachedKeyStore;
+      }
     }
 
-    /////
-    logger.debug('params', params);
+    let jwks;
+
+    if(jwksUri.startsWith('http://') || jwksUri.startsWith('https://')) {
+      try {
+        let rsp = await request.get(jwksUri);
+
+        jwks = rsp.body;
+      } catch(e) {
+        logger.error("Failed to get signing key", e);
+        throw e;
+      }
+    }
+
+    if(jwksUri.startsWith('file://')) {
+      try {
+        let path = jwksUri.replace('file://','');
+        const buffer = await fs.readFile(path);
+
+        jwks = JSON.parse(buffer.toString());
+      } catch(e) {
+        logger.error("Failed to get signing key", e);
+        throw e;
+      }
+    }
+
+    if(!jwks) {
+      throw new Error("Unknown handler for " + jwksUri);
+    }
+
+    cachedKeyStore = JWKS.asKeyStore(jwks);
+    expiresAt = new Date(now.getTime() + cacheTimeout);
+
+    return cachedKeyStore;
+  };
+
+  const verifyAndDecodeToken = async (access_token) => {
+    let keyStore = await getKeyStore();
+
+    try {
+      await JWT.verify(access_token, keyStore);
+    } catch(e) {
+      logger.error(e);
+      throw e;
+    }
+
+    let decoded = JWT.decode(access_token);
+
+    return decoded;
+  };
+
+  const authenticate = async (params) => {
     const token = getToken(params);
 
-    const decoded = jwt.decode(token, { complete: true });
-    if (!decoded || !decoded.header || !decoded.header.kid) {
-      throw new Error('invalid token');
-    }
+    const decoded = await verifyAndDecodeToken(token);
 
-    const client = jwksClient({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 10, // Default value
-      jwksUri: props.jwksUri,
-    });
-
-    const getSigningKey = util.promisify(client.getSigningKey);
-    return getSigningKey(decoded.header.kid)
-      .then((key) => {
-        const signingKey = key.publicKey || key.rsaPublicKey;
-        return jwt.verify(token, signingKey, jwtOptions);
-      })
-      .then((decoded)=> ({
-        principalId: decoded.sub,
-        policyDocument: getPolicyDocument('Allow', params.methodArn),
-        context: { scope: decoded.scope }
-      }));
+    return {
+      principalId: decoded.sub,
+      policyDocument: getPolicyDocument('Allow', params.methodArn),
+      context: { scope: decoded.scope }
+    };
   }
 
   return authenticate;
--- src/http/authorizer.spec.js
+++ src/http/authorizer.spec.js
@@ -9,12 +9,12 @@ const very_much_not_secure  = require('../../test/very_much_not_secure/jwks.json
 
 const Authorizer = require('./authorizer');
 
-describe('@wip authorizer', () => {
+describe('authorizer', () => {
   let app;
   let server;
   let authorizer;
 
-  before(async () => {
+  beforeEach(async () => {
     app = express();
 
     app.get('/.well-known/jwks.json', (req, res) => {
@@ -32,74 +32,171 @@ describe('@wip authorizer', () => {
     });
   });
 
-  after(async () => {
+  afterEach(async () => {
     if(server) {
       server.close();
     }
   });
 
-  describe('should reject', () => {
-    const test = (name, event) => {
-      it(name, async () => {
-        let failed = false;
-        try {
-          const result = await authorizer(event);
-        } catch(e) {
-          // console.error(e.message);
-          failed = true;
+  describe('with http', () => {
+    beforeEach(async () => {
+      authorizer = Authorizer({
+        audience: "fake",
+        issuer: "fake",
+        jwksUri: `http://localhost:${server.address().port}/.well-known/jwks.json`
+      });
+    });
+
+    describe('should reject', () => {
+      const test = (name, event) => {
+        it(name, async () => {
+          let failed = false;
+          try {
+            const result = await authorizer(event);
+          } catch(e) {
+            // console.error(e.message);
+            failed = true;
+          }
+
+          expect(failed).to.eql(true);
+        });
+      };
+
+      test('null', null);
+      test('empty object', {});
+      test('bad type', { type: "FOO" });
+      test('no token', { type: "TOKEN" });
+      test('invalid token match', { type: "TOKEN", authorizationToken: "Foo 1" });
+      test('invalid token match', { type: "TOKEN", authorizationToken: "Bearer zzz" });
+    });
+
+    describe('valid tokens', () => {
+      let token;
+      let result;
+
+      beforeEach(async () => {
+        if(result) {
+          return;
         }
 
-        expect(failed).to.eql(true);
+        let key = very_much_not_secure.keys[0]
+
+        token = JWT.sign({
+          sub: "123",
+          foo: "bar",
+          aud: "fake",
+          iss: "fake",
+          scope: "test"
+        }, key);
+
+        const event = {
+          type: "TOKEN",
+          authorizationToken: `Bearer ${token}`,
+          methodArn :"arn:aws:execute-api:us-west-2:123456789012:ymy8tbxw7b/*/GET/"
+        }
+
+        result = await authorizer(event);
       });
-    };
 
-    test('null', null);
-    test('empty object', {});
-    test('bad type', { type: "FOO" });
-    test('no token', { type: "TOKEN" });
-    test('invalid token match', { type: "TOKEN", authorizationToken: "Foo 1" });
-    test('invalid token match', { type: "TOKEN", authorizationToken: "Bearer zzz" });
+      it('policyDocument', async () => {
+        expect(result.policyDocument).to.be.ok;
+      });
+
+      it('principalId', async () => {
+        expect(result.principalId).to.be.eql("123");
+      });
+
+      it('scope', async () => {
+        expect(result.context.scope).to.eql("test");
+      });
+
+      it('scope', async () => {
+        expect(result.policyDocument.Statement[0].Resource).to.include("ymy8tbxw7b");
+      });
+    });
+
+
   });
 
-  describe('valid tokens', () => {
-    let token;
-    let result;
+  describe('with file', () => {
+    let fileAuthorizer;
 
-    before(async () => {
-      let key = very_much_not_secure.keys[0]
-
-      token = JWT.sign({
-        sub: "123",
-        foo: "bar",
-        aud: "fake",
-        iss: "fake",
-        scope: "test"
-      }, key);
-
-      const event = {
-        type: "TOKEN",
-        authorizationToken: `Bearer ${token}`,
-        methodArn :"arn:aws:execute-api:us-west-2:123456789012:ymy8tbxw7b/*/GET/"
-      }
-
-      result = await authorizer(event);
+    beforeEach(() => {
+      fileAuthorizer = Authorizer({
+        audience: "fake",
+        issuer: "fake-file",
+        jwksUri: "file://./test/very_much_not_secure/jwks.json"
+      });
     });
 
-    it('policyDocument', async () => {
-      expect(result.policyDocument).to.be.ok;
+    describe('should reject', () => {
+      const test = (name, event) => {
+        it(name, async () => {
+          let failed = false;
+          try {
+            const result = await authorizer(event);
+          } catch(e) {
+            // console.error(e.message);
+            failed = true;
+          }
+
+          expect(failed).to.eql(true);
+        });
+      };
+
+      test('null', null);
+      test('empty object', {});
+      test('bad type', { type: "FOO" });
+      test('no token', { type: "TOKEN" });
+      test('invalid token match', { type: "TOKEN", authorizationToken: "Foo 1" });
+      test('invalid token match', { type: "TOKEN", authorizationToken: "Bearer zzz" });
     });
 
-    it('principalId', async () => {
-      expect(result.principalId).to.be.eql("123");
-    });
 
-    it('scope', async () => {
-      expect(result.context.scope).to.eql("test");
-    });
 
-    it('scope', async () => {
-      expect(result.policyDocument.Statement[0].Resource).to.include("ymy8tbxw7b");
+    describe('valid tokens', () => {
+      let token;
+      let result;
+
+      beforeEach(async () => {
+        if(result) {
+          return;
+        }
+
+        let key = very_much_not_secure.keys[0]
+
+        token = JWT.sign({
+          sub: "123",
+          foo: "bar",
+          aud: "fake",
+          iss: "fake",
+          scope: "test"
+        }, key);
+
+        const event = {
+          type: "TOKEN",
+          authorizationToken: `Bearer ${token}`,
+          methodArn :"arn:aws:execute-api:us-west-2:123456789012:ymy8tbxw7b/*/GET/"
+        }
+
+        result = await fileAuthorizer(event);
+      });
+
+      it('policyDocument', async () => {
+        expect(result.policyDocument).to.be.ok;
+      });
+
+      it('principalId', async () => {
+        expect(result.principalId).to.be.eql("123");
+      });
+
+      it('scope', async () => {
+        expect(result.context.scope).to.eql("test");
+      });
+
+      it('scope', async () => {
+        expect(result.policyDocument.Statement[0].Resource).to.include("ymy8tbxw7b");
+      });
     });
   });
-
 });
