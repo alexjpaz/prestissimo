d682f01bfe76357c372657d78ee94f9018ca86b1
Alexander Paz <alexjpaz@gmail.com>
2020-05-27 14:31:50 -0400

    Fixing PATH?

--- src/http/index.js
+++ src/http/index.js
@@ -1,3 +1,10 @@
+if(process.env.LAMBDA_TASK_ROOT) {
+  // FIXME - Hacky!
+  process.env.PATH = `${process.env.LAMBDA_TASK_ROOT || './'}/opt`;
+
+  console.info("PATH=${process.env.PATH}");
+}
+
 const serverless = require('serverless-http');
 
 const Router = require('./router');
