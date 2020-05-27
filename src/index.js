513d761de66b75ee026c2476d3df7a0654db4cc6
Alexander Paz <alexjpaz@gmail.com>
2020-05-27 13:48:03 -0400

    Re enable PATH

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 if(process.env.LAMBDA_TASK_ROOT) {
   // FIXME - Hacky!
-  //process.env.PATH = `${process.env.LAMBDA_TASK_ROOT || './'}/opt`;
+  process.env.PATH = `${process.env.LAMBDA_TASK_ROOT || './'}/opt`;
+
+  console.info("PATH=${process.env.PATH}");
 }
 module.exports.convert = require('./convert').convert;
 module.exports.http = require('./http');
